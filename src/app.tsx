59be2c8daab0c8eb91a83c48fbe496eb532f91a1
Nosebones69 <ghostsoldier222@gmail.com>
2025-10-19 16:41:05 -0500

    Inicié todo

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EnergiaVIS from './energia_vis_platform'
+
+function App() {
+  return (
+    <div>
+      <h1>Proyecto Viviendas VIS</h1>
+      <EnergiaVIS />
+    </div>
+  )
+}
+
+export default App
